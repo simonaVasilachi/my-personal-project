a456f0efb9c3bc2a8a75edcd2dd99f9d855d5b4c
Simona Vasilachi <simona.vasilachi@ness.com>
2021-10-26 18:57:12 +0300

    in progress

--- src/app/Views/login.component/login.component.ts
+++ src/app/Views/login.component/login.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { UserInfo } from "src/app/Entities/userInfo.model";
 import { Store } from '@ngrx/store';
 import { login } from "src/app/Services/login.action";
 import { navigate } from "src/app/Services/route.actions";
+
 
 @Component(
     {
@@ -14,32 +15,41 @@ import { navigate } from "src/app/Services/route.actions";
 )
 
 export class LoginComponent implements OnInit {
-     hide=true;
-     
+    hide = true;
+    isDisabled: boolean = true;
 
 
-        loginForm = this.formBuilder.group({
-            userName: [''],
-            password: ['']
-            
-        })
+    loginForm: FormGroup = this.formBuilder.group({
+        userName: [''],
+        password: ['']
 
-        
+    })
+
+
 
     constructor(private formBuilder: FormBuilder,
         private store: Store
     ) { }
 
     ngOnInit() {
-     
+        //console.log(this.loginForm.controls.userName)
+        if (
+            this.loginForm.controls.userName.value === "" ||
+            this.loginForm.controls.password.value === ""
+        ) { this.isDisabled = true }
+        else {
+            this.isDisabled = false
+        }
+
+
     }
 
     onLogin() {
         const { userName, password } = this.loginForm.value;
         const userInfo = new UserInfo(userName, password);
         this.store.dispatch(login({ login: userInfo }));
-        this.store.dispatch(navigate({url: ''}))
-        
+        this.store.dispatch(navigate({ url: '' }))
+
     }
 }
 
