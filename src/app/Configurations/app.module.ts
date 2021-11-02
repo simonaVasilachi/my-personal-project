67641e1ed911abd9564bc25ec4a7790852dc458b
Simona Vasilachi <simona.vasilachi@ness.com>
2021-11-02 10:46:04 +0200

    cleaned comments, left only the used code for login view

--- src/app/Configurations/app.module.ts
+++ src/app/Configurations/app.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '../Views/app.component';
-import { UserListComponent } from '../Entities/userList.component';
-import { RouterEffects } from '../Services/route.effects';
-import { userReducer } from '../Services/user.reducer';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-
 
 
 @NgModule({
@@ -22,16 +14,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   imports: [
     BrowserModule,
     CommonModule,
-    StoreModule.forRoot({ user: userReducer }),
     AppRoutingModule,
-    EffectsModule.forRoot([RouterEffects]),
     BrowserAnimationsModule,
     MatToolbarModule
-
   ],
 
   declarations: [
-    UserListComponent,
     AppComponent
   ],
 
